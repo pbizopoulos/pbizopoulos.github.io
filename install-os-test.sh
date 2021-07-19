2442273d498d5a618ce31a604480768a7b5cfe3f
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2021-07-19 03:35:13 +0300

    use /var/tmp instead of creating a tmp directory

--- install-os-test.sh
+++ install-os-test.sh
@@ -22,36 +22,37 @@ function expect() {
 	done
 }
 
-LATEST_ISO="$(curl -fs "https://mirror.pkgbuild.com/iso/latest/" | grep -Eo 'archlinux-[0-9]{4}\.[0-9]{2}\.[0-9]{2}-x86_64.iso' | head -n 1)"
-curl -fO "https://mirror.pkgbuild.com/iso/latest/${LATEST_ISO}"
-ISO="${LATEST_ISO}"
+ISO="$(curl -fs "https://mirror.pkgbuild.com/iso/latest/" | grep -Eo 'archlinux-[0-9]{4}\.[0-9]{2}\.[0-9]{2}-x86_64.iso' | head -n 1)"
+if [ ! -f "/var/tmp/${ISO}" ]; then
+	curl -o "/var/tmp/${ISO}" "https://mirror.pkgbuild.com/iso/latest/${ISO}"
+fi
 
-rm -rf ./tmp/ && mkdir -p ./tmp/ && cd ./tmp/
-xorriso -osirrox on -indev "../${ISO}" -extract arch/boot/x86_64 .
-ISO_VOLUME_ID="$(xorriso -indev "../${ISO}" |& awk -F : '$1 ~ "Volume id" {print $2}' | tr -d "' ")"
-mkfifo guest.out guest.in
-qemu-img create -f qcow2 scratch-disk.img 16G
+rm -f /var/tmp/guest.in /var/tmp/guest.out
+xorriso -osirrox on -indev "/var/tmp/${ISO}" -extract arch/boot/x86_64 /var/tmp/
+ISO_VOLUME_ID="$(xorriso -indev "/var/tmp/${ISO}" |& awk -F : '$1 ~ "Volume id" {print $2}' | tr -d "' ")"
+mkfifo /var/tmp/guest.out /var/tmp/guest.in
+qemu-img create -f qcow2 /var/tmp/os.img 8G
 
 { qemu-system-x86_64 \
 	-m 4G \
 	-enable-kvm \
 	-net nic \
 	-net user \
-	-kernel vmlinuz-linux \
-	-initrd initramfs-linux.img \
+	-kernel /var/tmp/vmlinuz-linux \
+	-initrd /var/tmp/initramfs-linux.img \
 	-append "archisolabel=${ISO_VOLUME_ID} console=ttyS0" \
-	-drive file=scratch-disk.img,if=virtio \
-	-drive file="../${ISO}",if=virtio,media=cdrom,readonly=on \
+	-drive file=/var/tmp/os.img,if=virtio \
+	-drive file="/var/tmp/${ISO}",if=virtio,media=cdrom,readonly=on \
 	-monitor none \
-	-serial pipe:guest \
+	-serial pipe:/var/tmp/guest \
 	-nographic || kill "${$}"; } &
 
-exec 3>&1 {fd}< <(tee /dev/fd/3 <guest.out)
+exec 3>&1 {fd}< <(tee /dev/fd/3 </var/tmp/guest.out)
 expect "archiso login:"
-echo -en "root\n" > guest.in
+echo -en "root\n" > /var/tmp/guest.in
 expect "# "
-echo -en "curl -L pbizopoulos.github.io/install-os.sh | sed 's/sda/vda/' | bash\n" > guest.in
+echo -en "curl -L pbizopoulos.github.io/install-os.sh | sed 's/sda/vda/' | bash\n" > /var/tmp/guest.in
 expect "7. Add contents of /home/pbizopoulos/.ssh/id_ed25519.pub to GitHub SSH settings."
-echo -en "shutdown now\n" > guest.in
+echo -en "shutdown now\n" > /var/tmp/guest.in
 wait
-qemu-system-x86_64 -m 4G -enable-kvm -drive file=scratch-disk.img -drive if=pflash,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.fd
+qemu-system-x86_64 -m 4G -enable-kvm -drive file=/var/tmp/os.img -drive if=pflash,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.fd
