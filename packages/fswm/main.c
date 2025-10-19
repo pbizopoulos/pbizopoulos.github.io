5662523578d0380d40cc00c7d57d7f17621f3685
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-19 22:07:31 +0300

    .

--- packages/fswm/main.c
+++ packages/fswm/main.c
@@ -13,7 +13,8 @@ static int focused_index = 0;
 static void focus_client_at_index(xcb_connection_t *conn, int target_index)
 {
     uint32_t stack_mode = XCB_STACK_MODE_ABOVE;
-    if (num_clients == 0) return;
+    if (num_clients == 0)
+        return;
     focused_index = (target_index % num_clients + num_clients) % num_clients;
     xcb_configure_window(conn, clients[focused_index], XCB_CONFIG_WINDOW_STACK_MODE, &stack_mode);
     xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, clients[focused_index], XCB_CURRENT_TIME);
@@ -29,13 +30,17 @@ int main(int argc, char *argv[])
     uint32_t screen_geometry[4];
     int i, j;
     const xcb_keycode_t KEY_TAB = 23;
-    const xcb_keycode_t KEY_T = 28;
+    const xcb_keycode_t KEY_T   = 28;
     const xcb_keycode_t KEY_DEL = 119;
-    if (argc < 2) return EXIT_FAILURE;
+    if (argc < 2)
+        return EXIT_FAILURE;
     conn = xcb_connect(NULL, NULL);
-    if (!conn || xcb_connection_has_error(conn)) return EXIT_FAILURE;
+    if (!conn || xcb_connection_has_error(conn))
+        return EXIT_FAILURE;
     screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-    event_mask = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE;
+    event_mask = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+                 XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
+                 XCB_EVENT_MASK_PROPERTY_CHANGE;
     if (xcb_request_check(conn, xcb_change_window_attributes_checked(conn, screen->root, XCB_CW_EVENT_MASK, &event_mask))) {
         xcb_disconnect(conn);
         return EXIT_FAILURE;
@@ -52,46 +57,56 @@ int main(int argc, char *argv[])
     while (1) {
         xcb_flush(conn);
         event = xcb_wait_for_event(conn);
-        if (!event) break;
+        if (!event)
+            break;
         switch (event->response_type & ~0x80) {
             case XCB_KEY_PRESS: {
                 xcb_key_press_event_t *key_event = (xcb_key_press_event_t *)event;
+                int direction;
                 if ((key_event->state & XCB_MOD_MASK_1) && key_event->detail == KEY_TAB) {
-                    int direction = (key_event->state & XCB_MOD_MASK_SHIFT) ? -1 : 1;
+                    direction = (key_event->state & XCB_MOD_MASK_SHIFT) ? -1 : 1;
                     focus_client_at_index(conn, focused_index + direction);
-                }
-                else if ((key_event->state & (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1)) == (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1) && key_event->detail == KEY_T) {
-                    if (!fork()) execvp(argv[1], &argv[1]);
-                }
-                else if ((key_event->state & (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1)) == (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1) && key_event->detail == KEY_DEL) {
-                    xcb_disconnect(conn);
-                    free(event);
-                    return 0;
+                } else if ((key_event->state & (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1)) == (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1)) {
+                    if (key_event->detail == KEY_T) {
+                        if (!fork())
+                            execvp(argv[1], &argv[1]);
+                    } else if (key_event->detail == KEY_DEL) {
+                        xcb_disconnect(conn);
+                        free(event);
+                        return 0;
+                    }
                 }
                 break;
             }
             case XCB_MAP_REQUEST: {
                 xcb_map_request_event_t *map_event = (xcb_map_request_event_t *)event;
-                if (num_clients < MAX_CLIENTS) clients[num_clients++] = map_event->window;
+                if (num_clients < MAX_CLIENTS)
+                    clients[num_clients++] = map_event->window;
                 xcb_map_window(conn, map_event->window);
                 focus_client_at_index(conn, num_clients - 1);
-                xcb_configure_window(conn, clients[focused_index], XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, screen_geometry);
+                xcb_configure_window(conn, clients[focused_index],
+                                     XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
+                                     screen_geometry);
                 break;
             }
             case XCB_UNMAP_NOTIFY: {
                 xcb_unmap_notify_event_t *unmap_event = (xcb_unmap_notify_event_t *)event;
                 for (i = 0; i < num_clients; i++) {
                     if (clients[i] == unmap_event->window) {
-                        for (j = i; j < num_clients - 1; j++) clients[j] = clients[j + 1];
+                        for (j = i; j < num_clients - 1; j++)
+                            clients[j] = clients[j + 1];
                         num_clients--;
                         break;
                     }
                 }
-                if (num_clients > 0) focus_client_at_index(conn, focused_index);
-                else focused_index = 0;
+                if (num_clients > 0)
+                    focus_client_at_index(conn, focused_index);
+                else
+                    focused_index = 0;
                 break;
             }
-            default: break;
+            default:
+                break;
         }
         free(event);
     }
