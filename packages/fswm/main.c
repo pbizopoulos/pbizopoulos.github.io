b18b1e5d681bb3b0e0bdb8616ce45a8d5f66ab93
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-19 20:39:18 +0300

    .

--- packages/fswm/main.c
+++ packages/fswm/main.c
@@ -13,9 +13,10 @@ static int current_index = 0;
 static void focus_client(xcb_connection_t *conn, int index) {
     uint32_t stack = XCB_STACK_MODE_ABOVE;
     if (client_count == 0) return;
-    current_index = (index + client_count) % client_count;
+    current_index = (index % client_count + client_count) % client_count;
     xcb_configure_window(conn, clients[current_index], XCB_CONFIG_WINDOW_STACK_MODE, &stack);
     xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, clients[current_index], XCB_CURRENT_TIME);
+    xcb_flush(conn);
 }
 
 int main(int argc, char *argv[]) {
@@ -37,7 +38,9 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
     screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-    event_mask = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE;
+    event_mask = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+                 XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
+                 XCB_EVENT_MASK_PROPERTY_CHANGE;
     if (xcb_request_check(conn,
         xcb_change_window_attributes_checked(conn, screen->root, XCB_CW_EVENT_MASK, &event_mask))) {
         fprintf(stderr, "fswm: another window manager is running\n");
@@ -49,10 +52,14 @@ int main(int argc, char *argv[]) {
     cfg[1] = 0;
     cfg[2] = screen->width_in_pixels;
     cfg[3] = screen->height_in_pixels;
-    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_1, tab, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
-    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_1 | XCB_MOD_MASK_SHIFT, tab, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
-    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1, t, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
-    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1, del, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
+    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_1, tab,
+                 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
+    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_1 | XCB_MOD_MASK_SHIFT, tab,
+                 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
+    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1, t,
+                 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
+    xcb_grab_key(conn, 1, screen->root, XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1, del,
+                 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
     while (1) {
         xcb_flush(conn);
         ev = xcb_wait_for_event(conn);
@@ -66,12 +73,12 @@ int main(int argc, char *argv[]) {
                         next = current_index - 1;
                     else
                         next = current_index + 1;
+                    focus_client(conn, next);
                 } else if (kp->detail == del) {
                     break;
                 } else if (kp->detail == t && (kp->state & (XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_1))) {
                     if (!fork()) execvp(argv[1], &argv[1]);
                 }
-                if (next != -1) focus_client(conn, next);
                 break;
             }
             case XCB_MAP_REQUEST: {
@@ -80,7 +87,11 @@ int main(int argc, char *argv[]) {
                     clients[client_count++] = me->window;
                 xcb_map_window(conn, me->window);
                 focus_client(conn, client_count - 1);
-                xcb_configure_window(conn, clients[current_index], XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, cfg);
+                xcb_configure_window(conn, clients[current_index],
+                                     XCB_CONFIG_WINDOW_X |
+                                     XCB_CONFIG_WINDOW_Y |
+                                     XCB_CONFIG_WINDOW_WIDTH |
+                                     XCB_CONFIG_WINDOW_HEIGHT, cfg);
                 break;
             }
             case XCB_UNMAP_NOTIFY: {
@@ -91,12 +102,13 @@ int main(int argc, char *argv[]) {
                         for (j = i; j < client_count - 1; j++)
                             clients[j] = clients[j + 1];
                         client_count--;
-                        if (current_index >= client_count)
-                            current_index = client_count - 1;
                         break;
                     }
                 }
-                focus_client(conn, current_index);
+                if (client_count > 0)
+                    focus_client(conn, current_index);
+                else
+                    current_index = 0;
                 break;
             }
             default:
