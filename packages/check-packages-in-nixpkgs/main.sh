159447287695492be1bb664e90aae07e7d4a90e7
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-21 21:10:12 +0300

    .

--- packages/check-packages-in-nixpkgs/main.sh
+++ packages/check-packages-in-nixpkgs/main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-packages="autogluon coreforecast efficientnet-pytorch flwr flwr-datasets iterators jsf label-studio-sdk mediapipe mlforecast onnx-ir onnxscript pretrainedmodels scikit-spatial segmentation_models_pytorch sqladmin utilsforecast wfdb"
+packages="efficientnet-pytorch jsf label-studio-sdk mediapipe pretrainedmodels scikit-spatial segmentation_models_pytorch sqladmin wfdb"
 
 for pkg in $packages; do
   exists=$(nix-instantiate -I nixpkgs=channel:nixos-unstable --eval -E "
--- packages/check-pip-latest-version/main.sh
+++ packages/check-pip-latest-version/main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-packages="autogluon label-studio-sdk onnxscript segmentation_models_pytorch"
+packages="label-studio-sdk segmentation_models_pytorch"
 
 for package in $packages; do
   nix run nixpkgs#python312Packages.pip index versions "$package" | sed -n '1p'
