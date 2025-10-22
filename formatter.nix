e37b2d8d0fd19bad158184e3faad2acec74ef942
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-22 15:49:11 +0300

    .

--- formatter.nix
+++ formatter.nix
@@ -1,49 +1,63 @@
-{ inputs, pkgs, ... }:
-inputs.treefmt-nix.lib.mkWrapper pkgs {
-  programs = {
-    actionlint.enable = true;
-    beautysh.enable = true;
-    biome.enable = true;
-    clang-format.enable = true;
-    deadnix.enable = true;
-    hlint.enable = true;
-    nixfmt = {
-      enable = true;
-      strict = true;
-    };
-    ormolu.enable = true;
-    prettier.enable = true;
-    ruff-check = {
-      enable = true;
-      extendSelect = [ "ALL" ];
-    };
-    ruff-format.enable = true;
-    shellcheck.enable = true;
-    shfmt = {
-      enable = true;
-      simplify = true;
-    };
-    statix.enable = true;
-    yamlfmt.enable = true;
-  };
-  projectRootFile = "flake.nix";
-  settings = {
-    formatter = {
-      mypy = {
-        command = pkgs.mypy;
-        includes = [ "*.py" ];
-        options = [
-          "--explicit-package-bases"
-          "--ignore-missing-imports"
-          "--strict"
-        ];
+{
+  flake,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  formatter = treefmtEval.config.build.wrapper;
+  treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+    programs = {
+      actionlint.enable = true;
+      beautysh.enable = true;
+      biome.enable = true;
+      clang-format.enable = true;
+      deadnix.enable = true;
+      hlint.enable = true;
+      nixfmt = {
+        enable = true;
+        strict = true;
       };
-      ruff-check.options = [ "--unsafe-fixes" ];
-      shfmt.options = [ "--posix" ];
+      ormolu.enable = true;
+      prettier.enable = true;
+      ruff-check = {
+        enable = true;
+        extendSelect = [ "ALL" ];
+      };
+      ruff-format.enable = true;
+      shellcheck.enable = true;
+      shfmt = {
+        enable = true;
+        simplify = true;
+      };
+      statix.enable = true;
+      yamlfmt.enable = true;
     };
-    global.excludes = [
-      "*/prm/**"
-      "*/tmp/**"
-    ];
+    projectRootFile = "flake.nix";
+    settings = {
+      formatter = {
+        mypy = {
+          command = pkgs.mypy;
+          includes = [ "*.py" ];
+          options = [
+            "--explicit-package-bases"
+            "--ignore-missing-imports"
+            "--strict"
+          ];
+        };
+        ruff-check.options = [ "--unsafe-fixes" ];
+        shfmt.options = [ "--posix" ];
+      };
+      global.excludes = [
+        "*/prm/**"
+        "*/tmp/**"
+      ];
+    };
+  };
+in
+formatter
+// {
+  passthru = formatter.passthru // {
+    tests.check = treefmtEval.config.build.check flake;
   };
 }
