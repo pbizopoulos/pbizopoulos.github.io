32f53b6f0c1445a73500c2b9682066437fe46132
Paschalis Bizopoulos <pbizop@gmail.com>
2025-11-05 10:46:57 +0200

    fix install-nixos

--- hosts/install-iso/configuration.nix
+++ hosts/install-iso/configuration.nix
@@ -1,26 +1,32 @@
 { modulesPath, pkgs, ... }:
+
 let
   installNixos = pkgs.writeShellApplication {
     name = "install-nixos";
     runtimeInputs = [
       pkgs.disko
       pkgs.git
+      pkgs.mkpasswd
       pkgs.nixos-install
     ];
     text = ''
-      if [ "$1" = "--help" ]; then
-        echo "1. sudo mount /dev/sdXN /mnt"
-        echo "2. git -C /mnt/<username> archive -o ~/repo.tar.gz"
-        echo "3. sudo umount /mnt"
-        echo "4. install-nixos <hostname>"
-        echo "5. tar xzf ~/repo.tar.gz -C /mnt/home/<username>"
-        echo "6. git -C /mnt/home/<username> add -f ."
+      set -euo pipefail
+      if [ "$#" -ne 3 ]; then
+        echo "Usage: install-nixos <hostname> <username> <disk>" >&2
         exit 1
       fi
-      disko --flake "github:pbizopoulos/pbizopoulos.github.io#$1" --mode disko
-      nixos-install --flake "github:pbizopoulos/pbizopoulos.github.io#$1" --no-root-passwd
+      hostname="$1"
+      username="$2"
+      disk="$3"
+      sudo mount "${disk}" /mnt
+      git -C "/mnt/${username}" archive -o ~/repo.tar.gz HEAD
+      sudo umount /mnt
+      disko --flake "github:pbizopoulos/pbizopoulos.github.io#${hostname}" --mode disko
+      nixos-install --flake "github:pbizopoulos/pbizopoulos.github.io#${hostname}" --no-root-passwd
       mkdir -p /mnt/persistent/passwords
-      mkpasswd -m sha-512 >/mnt/persistent/passwords/pbizopoulos
+      mkpasswd -m sha-512 >"/mnt/persistent/passwords/${username}"
+      tar xzf ~/repo.tar.gz -C "/mnt/home/${username}"
+      git -C "/mnt/home/${username}" add -f .
     '';
   };
 in
@@ -38,6 +44,6 @@ in
     allowUnfree = true;
     permittedInsecurePackages = [ "broadcom-sta-6.30.223.271-57-6.12.53" ];
   };
-  users.motd = "Run 'install-nixos'";
+  users.motd = "Run 'install-nixos <hostname> <username> <disk>'";
   virtualisation.vmVariant.virtualisation.graphics = false;
 }
