3b92404ab8807c5e380a67799eeb67a2e413c8b6
Paschalis Bizopoulos <pbizop@gmail.com>
2025-11-05 18:30:07 +0200

    .

--- packages/bootstrap-git-submodules/main.sh
+++ packages/bootstrap-git-submodules/main.sh
@@ -4,7 +4,7 @@ set -e
 repository_domain=github.com
 git init "$HOME"
 printf '*\n!.gitignore\n' >"$HOME/.gitignore"
-gh auth status --hostname "$repository_domain" || gh auth login --git-protocol ssh --hostname "$repository_domain" --skip-ssh-key --web
+gh auth status --hostname "$repository_domain" || gh auth login --git-protocol ssh --hostname "$repository_domain" --web
 gh repo list | while read -r repo _; do
   git -C "$HOME" submodule add --force git@"$repository_domain:$repo" "$repository_domain/$repo"
 done
