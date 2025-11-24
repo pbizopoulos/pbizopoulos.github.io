a9bfc3b806d672bc35d278ad427e15a9a4c2a90f
Paschalis Bizopoulos <pbizop@gmail.com>
2025-11-24 17:11:09 +0200

    .

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -70,31 +70,7 @@
   };
   hardware = {
     nvidia.open = true;
-    nvidia-container-toolkit = {
-      enable = true;
-      extraArgs = [
-        "--disable-hook"
-        "create-symlinks"
-      ];
-      package = pkgs.nvidia-container-toolkit.overrideAttrs (_old: {
-        postPatch = ''
-          substituteInPlace internal/config/config.go \
-            --replace-fail '/usr/bin/nvidia-container-runtime-hook' "$tools/bin/nvidia-container-runtime-hook" \
-            --replace-fail '/sbin/ldconfig' '${pkgs.glibc.bin}/sbin/ldconfig'
-          # substituteInPlace tools/container/toolkit/toolkit.go \
-          #   --replace-fail '/sbin/ldconfig' '${pkgs.glibc.bin}/sbin/ldconfig'
-          substituteInPlace cmd/nvidia-cdi-hook/update-ldcache/update-ldcache.go \
-            --replace-fail '/sbin/ldconfig' '${pkgs.glibc.bin}/sbin/ldconfig'
-        '';
-        src = pkgs.fetchFromGitHub {
-          hash = "sha256-y81UbNoMfIhl9Rf1H3RTRmGR3pysDtKlApLrIxwou9I=";
-          owner = "nvidia";
-          repo = "nvidia-container-toolkit";
-          rev = "08b3a388e7b1d447e10d4c4d4a71dca29a98a964";
-        };
-        version = "git";
-      });
-    };
+    nvidia-container-toolkit.enable = true;
   };
   imports = [
     ./hardware-configuration.nix
