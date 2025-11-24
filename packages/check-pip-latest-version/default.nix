6b52c06264e3e09bf45ca2570934423cac748a1b
Paschalis Bizopoulos <pbizop@gmail.com>
2025-11-24 17:03:19 +0200

    .

--- packages/check-pip-latest-version/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-pkgs.writeShellApplication {
-  name = builtins.baseNameOf ./.;
-  text = builtins.readFile ./main.sh;
-}
--- packages/check-pip-latest-version/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-packages="segmentation_models_pytorch"
-
-for package in $packages; do
-  nix run nixpkgs#python312Packages.pip index versions "$package" | sed -n '1p'
-done
