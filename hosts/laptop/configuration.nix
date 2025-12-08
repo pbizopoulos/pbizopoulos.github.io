938cf611706f71837d2f3bddeb20c22cb725f4bb
Paschalis Bizopoulos <pbizop@gmail.com>
2025-12-08 15:04:28 +0200

    .

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -83,7 +83,7 @@
   ];
   nixpkgs.config = {
     allowUnfree = true;
-    permittedInsecurePackages = [ "broadcom-sta-6.30.223.271-59-6.12.60" ];
+    permittedInsecurePackages = [ "broadcom-sta-6.30.223.271-59-6.12.59" ];
   };
   preservation = {
     enable = true;
