047853739948218521d11c0183d1a7182d34cc05
Paschalis Bizopoulos <pbizop@gmail.com>
2026-01-12 10:46:43 +0200

    .

--- packages/check_repository_directory_structure/default.nix
+++ packages/check_repository_directory_structure/default.nix
@@ -2,7 +2,7 @@
   pkgs ? import <nixpkgs> { },
 }:
 pkgs.python312Packages.buildPythonPackage rec {
-  installPhase = ''mkdir -p $out/bin && cp ./main.py $out/bin/${pname}'';
+  installPhase = "mkdir -p $out/bin && cp ./main.py $out/bin/${pname}";
   meta.mainProgram = pname;
   pname = builtins.baseNameOf src;
   propagatedBuildInputs = [
