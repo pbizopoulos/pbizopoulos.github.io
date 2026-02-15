8af70f7e6dda80a174cc238ce48259db8b45055c
Paschalis Bizopoulos <pbizop@gmail.com>
2026-02-15 11:40:02 +0200

    .

--- packages/schemaui/default.nix
+++ packages/schemaui/default.nix
@@ -1,23 +1,15 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.stdenv.mkDerivation rec {
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp ${pname} $out/bin/${pname}
-    runHook postInstall
-  '';
-  meta.mainProgram = pname;
-  nativeBuildInputs = [
-    pkgs.autoPatchelfHook
-    pkgs.libgcc
-  ];
+pkgs.rustPlatform.buildRustPackage {
+  cargoHash = "sha256-wH1/8qs1inubnDAJnLyZY4g7rHXC6Yzdu+IWPO9A0Dc=";
+  doCheck = false;
   pname = "schemaui";
-  sourceRoot = ".";
-  src = pkgs.fetchurl {
-    sha256 = "sha256-Q/q1bf7olOMvu/8iQTull7dgYA25Q58lPXq0RrBgHe8=";
-    url = "https://github.com/YuniqueUnic/schemaui/releases/download/schemaui-cli-v0.2.7/schemaui-x86_64-unknown-linux-gnu.tar.gz";
+  src = pkgs.fetchFromGitHub {
+    owner = "YuniqueUnic";
+    repo = "schemaui";
+    rev = "bbb917f4e4aff2611aa33b2eef11f30262368815";
+    sha256 = "sha256-fLInoUQYxv8ilZWTWTFSKdKX7rgaoNe8dNMH5+AIGJU=";
   };
-  version = "0.2.7";
+  version = "latest";
 }
