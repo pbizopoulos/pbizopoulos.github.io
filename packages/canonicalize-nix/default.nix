9284bae484173db11bf7360739915e26817b95f0
Paschalis Bizopoulos <pbizop@gmail.com>
2026-02-19 15:31:00 +0200

    .

--- packages/canonicalize-nix/default.nix
+++ packages/canonicalize-nix/default.nix
@@ -1,33 +1,21 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.haskellPackages.callPackage (
-  {
-    HUnit,
-    base,
-    data-fix,
-    hnix,
-    mkDerivation,
-    prettyprinter,
-    temporary,
-    text,
-  }:
-  mkDerivation rec {
-    description = "Sorts attributes alphabetically, using dotted notation for attributes with sets or lists, and nested notation otherwise";
-    executableHaskellDepends = [
-      HUnit
-      base
-      data-fix
-      hnix
-      prettyprinter
-      temporary
-      text
-    ];
-    isExecutable = true;
-    license = pkgs.lib.licenses.mit;
-    mainProgram = "${pname}";
-    pname = builtins.baseNameOf src;
-    src = ./.;
-    version = "0.0.0";
-  }
-) { }
+pkgs.haskellPackages.mkDerivation rec {
+  description = "Sorts attributes alphabetically, using dotted notation for attributes with sets or lists, and nested notation otherwise";
+  executableHaskellDepends = [
+    pkgs.haskellPackages.HUnit
+    pkgs.haskellPackages.base
+    pkgs.haskellPackages.data-fix
+    pkgs.haskellPackages.hnix
+    pkgs.haskellPackages.prettyprinter
+    pkgs.haskellPackages.temporary
+    pkgs.haskellPackages.text
+  ];
+  isExecutable = true;
+  license = pkgs.lib.licenses.mit;
+  mainProgram = "${pname}";
+  pname = builtins.baseNameOf ./.;
+  src = ./.;
+  version = "0.0.0";
+}
