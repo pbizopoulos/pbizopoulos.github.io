a9d81fd5f05d14dfd7953a86a87c01dd6a857f73
Paschalis Bizopoulos <pbizop@gmail.com>
2026-02-26 14:10:33 +0200

    .

--- templates/nextjs-supabase/flake.nix
+++ templates/nextjs-supabase/flake.nix
@@ -1,38 +1,19 @@
 {
   inputs = {
-    agenix = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:ryantm/agenix";
-    };
     blueprint = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/blueprint";
     };
-    disko = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/disko";
-    };
-    preservation.url = "github:nix-community/preservation";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     treefmt-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/treefmt-nix";
     };
   };
-  outputs = inputs:
-    let
-      blueprintOutputs = inputs.blueprint {
-        inherit inputs;
-        nixpkgs.config.allowUnfree = true;
-      };
-    in
-      blueprintOutputs
-      // {
-        templates = {
-          default = {
-            path = ./.;
-            description = "Project template based on this repository";
-          };
-        };
-      };
+  outputs =
+    inputs:
+    inputs.blueprint {
+      inherit inputs;
+      nixpkgs.config.allowUnfree = true;
+    };
 }
