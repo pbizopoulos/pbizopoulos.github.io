e676477f4c18a17fd34cec99cc1cdc46a5445fb6
Paschalis Bizopoulos <pbizop@gmail.com>
2026-02-27 13:36:40 +0200

    .

--- templates/nextjs-supabase/test.sh
+++ templates/nextjs-supabase/test.sh
@@ -4,16 +4,14 @@ set -e
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 cd "$ROOT_DIR"
 
-# Clean
 rm -rf packages/default/node_modules/ packages/default/.next/
 find . -exec nix run github:pbizopoulos/pbizopoulos.github.io#remove-empty-lines {} + || true
 find . -exec nix run github:pbizopoulos/pbizopoulos.github.io#uncomment -- --remove-doc {} +
 nix fmt
-
-# Check
+nix build
 nix develop -c bash -c "
 set -e
-cd "$ROOT_DIR/packages/default/"
+cd $ROOT_DIR/packages/default/
 supabase stop
 supabase start
 npm install
@@ -26,6 +24,3 @@ npm run test:deps
 npm run test:mutation
 npm run test:mutation:metrics
 "
-
-# Build
-nix build
