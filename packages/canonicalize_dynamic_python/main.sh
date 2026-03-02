7a57117377eb9f44130a6ebae76221c6fedad9f1
Paschalis Bizopoulos <pbizop@gmail.com>
2026-03-02 14:28:43 +0200

    .

--- packages/canonicalize_dynamic_python/main.sh
+++ packages/canonicalize_dynamic_python/main.sh
@@ -2,14 +2,12 @@
 
 main_py="$(realpath "${1%%#*}")/packages/${1##*#}/main.py"
 coverage_dir="$(dirname "$main_py")/tmp/coverage"
+scalene_dir="$(dirname "$main_py")/tmp/scalene"
+mkdir -p "$scalene_dir"
 # shellcheck disable=SC2016
-nix develop "$1" --command bash -c '
+nix develop "$1" --command nix-shell -p python312Packages.coverage python312Packages.scalene --command '
   set -eu
   DEBUG=1 coverage run --data-file="'"$coverage_dir"'/.coverage" "'"$main_py"'"
   coverage html --data-file="'"$coverage_dir"'/.coverage" --directory="'"$coverage_dir"'" --ignore-errors
-'
-scalene_dir="$(dirname "$main_py")/tmp/scalene"
-mkdir -p "$scalene_dir"
-nix develop "$1" --command nix-shell -p python312Packages.scalene --command '
   DEBUG=1 scalene --outfile "'"$scalene_dir"'/profile.html" "'"$main_py"'"
 '
