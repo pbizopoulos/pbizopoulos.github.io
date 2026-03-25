bd9c2c62fef2e756513833a6a2f7d12c587ef05a
Paschalis Bizopoulos <pbizop@gmail.com>
2026-03-25 13:59:04 +0200

    .

--- hosts/install-iso/configuration.nix
+++ hosts/install-iso/configuration.nix
@@ -52,7 +52,7 @@ in
   nixpkgs.config = {
     allowUnfree = true;
     permittedInsecurePackages = [
-      "broadcom-sta-6.30.223.271-59-6.18.18"
+      "broadcom-sta-6.30.223.271-59-6.18.19"
     ];
   };
   users.motd = "Run 'install-nixos <hostname> <username> <disk>'";
