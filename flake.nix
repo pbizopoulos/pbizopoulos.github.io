efcabaf1a01fdaee7860f487122ffa72bc18a0c2
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-21 09:09:55 +0300

    .

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       nixpkgs.config = {
         allowUnfree = true;
         permittedInsecurePackages = [
-          "broadcom-sta-6.30.223.271-59-6.18.22"
+          "broadcom-sta-6.30.223.271-59-6.18.23"
         ];
       };
     }
