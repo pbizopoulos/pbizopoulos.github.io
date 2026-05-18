44ee652e1edc0b2e33f902f24f8dfaf260a77687
Paschalis Bizopoulos <pbizop@gmail.com>
2026-05-18 23:59:54 +0300

    .

--- packages/cv/default.nix
+++ packages/cv/default.nix
@@ -1,10 +1,11 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.writeShellScriptBin (baseNameOf ./.) ''
-  if [ "$DEBUG" = "1" ]; then
-    echo "debug mode: skipping http-server"
-    exit 0
+let
+  pname = baseNameOf ./.;
+in
+pkgs.writeShellScriptBin pname ''
+  if [ "$DEBUG" != "1" ]; then
+    exec ${pkgs.http-server}/bin/http-server ${./.} "$@"
   fi
-  exec ${pkgs.http-server}/bin/http-server ${./.} "$@"
 ''
