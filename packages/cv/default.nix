33996bf8b1a24aae2a3b14d9afbaea7792d94cee
Paschalis Bizopoulos <pbizop@gmail.com>
2026-06-04 17:16:08 +0300

    .

--- packages/cv/default.nix
+++ packages/cv/default.nix
@@ -5,7 +5,5 @@ let
   pname = baseNameOf ./.;
 in
 pkgs.writeShellScriptBin pname ''
-  if [ "$DEBUG" != "1" ]; then
-    exec ${pkgs.http-server}/bin/http-server ${./.} "$@"
-  fi
+  exec ${pkgs.http-server}/bin/http-server ${./.} "$@"
 ''
