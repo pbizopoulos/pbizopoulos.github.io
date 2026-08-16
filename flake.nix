e24abbb3c043f10674f40535bb998ca0abae536d
Paschalis Bizopoulos <pbizop@gmail.com>
2026-08-16 13:16:23 +0300

    .

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/disko";
     };
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixpkgs.follows = "canonicalization/nixpkgs";
     preservation.url = "github:nix-community/preservation";
   };
